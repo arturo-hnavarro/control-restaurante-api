1913914a55c0182e5e9b42774df6ae41444a8f36
arturohernandez-aeropost <arturo.hernandez@aeropost.com>
2021-04-13 11:14:45 -0600

    guarda, edita y obtiene un platillo por id

--- src/main/java/com/castrocarazo/restaurante/controllers/MenuController.java
+++ src/main/java/com/castrocarazo/restaurante/controllers/MenuController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.castrocarazo.restaurante.dao.IMenuDao;
@@ -29,6 +32,26 @@ public class MenuController {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
+	}
+	
+	@PostMapping(consumes = "application/json")
+	public ResponseEntity<Platillo> guardar(@RequestBody Platillo platillo){
+		try{
+			Platillo respuesta  = menu.save(platillo);
+			return new ResponseEntity<>(respuesta, HttpStatus.OK);
+		}catch(Exception ex) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/ver")
+	public ResponseEntity<Platillo> obtenerPorId(@RequestParam Long id){
+		try{
+			Platillo platillo  = menu.findById(id).orElse(null);
+			return new ResponseEntity<>(platillo, HttpStatus.OK);
+		}catch(Exception ex) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
