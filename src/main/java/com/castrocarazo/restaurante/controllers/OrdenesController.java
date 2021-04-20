b47f70b7df3c4bb6bd6e8f67cb0b2fddcda9e929
arturohernandez-aeropost <arturo.hernandez@aeropost.com>
2021-04-20 08:59:40 -0600

    Actualiza el disponible en el inventario de CatalogoIngredientes luego de colocar las ordenes

--- src/main/java/com/castrocarazo/restaurante/controllers/OrdenesController.java
+++ src/main/java/com/castrocarazo/restaurante/controllers/OrdenesController.java
@@ -1,7 +1,6 @@
 package com.castrocarazo.restaurante.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.castrocarazo.restaurante.dao.IOrdenDao;
-import com.castrocarazo.restaurante.domain.EstadoPlatillo;
 import com.castrocarazo.restaurante.domain.OrdenDeComida;
 import com.castrocarazo.restaurante.service.OrdenesDaoService;
 
--- src/main/java/com/castrocarazo/restaurante/service/OrdenesDaoService.java
+++ src/main/java/com/castrocarazo/restaurante/service/OrdenesDaoService.java
@@ -1,6 +1,5 @@
 package com.castrocarazo.restaurante.service;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,7 @@ import com.castrocarazo.restaurante.domain.CatalogoIngredientes;
 import com.castrocarazo.restaurante.domain.EstadoPlatillo;
 import com.castrocarazo.restaurante.domain.ItemOrden;
 import com.castrocarazo.restaurante.domain.OrdenDeComida;
-
+import com.castrocarazo.restaurante.domain.Platillo;
 
 @Service
 public class OrdenesDaoService {
@@ -21,7 +20,6 @@ public class OrdenesDaoService {
 	IOrdenDao ordenesService;
 	@Autowired
 	ICatalogoIngredientesDao inventario;
-	
 	@Autowired
 	MenuDaoService productos;
 
@@ -33,18 +31,16 @@ public class OrdenesDaoService {
 		return ordenesService.findById(id).orElse(null);
 	}
 
+	/*
+	 * Metodo encargado de almacenar una orden en BD y disminuir la cantidad disponible en el inventario
+	 */
 	public OrdenDeComida save(OrdenDeComida orden) {
 		OrdenDeComida response = null;
 		if (orden.getId() == null)
 			orden.setEstadoPlatillo(
 					new EstadoPlatillo(Long.valueOf(3), "solicitada", "Solicitada la orden por el cliente."));
-
-		/*for (ItemOrden item : orden.getItems()) {
-			actualizarCantidadEnInventario(item.getPlatillo().getIngredientes());// Actualizo el disponible en el inventario(catalogo)
-		}*/
-
 		response = ordenesService.save(orden);
-		
+		actualizarInventario(orden.getItems());
 		return response;
 	}
 
@@ -52,20 +48,22 @@ public class OrdenesDaoService {
 		ordenesService.deleteById(id);
 	}
 
-	// Utilitarios
-	private void actualizarCantidadEnInventario(List<CatalogoIngredientes> ingredientes) {
-		for (CatalogoIngredientes catalogoIngredientes : ingredientes) {
-			actualizarCantidadEnInventario(catalogoIngredientes.getId(), catalogoIngredientes.getCandidadDisponible());
-		}
-	}
+	
+	/**
+	 * Actualiza el disponible en el inventario de CatalogoIngredientes
+	 * @param items
+	 */
+	private void actualizarInventario(List<ItemOrden> items) {
+		for (ItemOrden item : items) {
+			Platillo platillo = productos.findById(item.getPlatillo().getId());
 
-	private void actualizarCantidadEnInventario(Long id, int cantidadSolicitada) {
-		CatalogoIngredientes item = inventario.findById(id).orElse(null);
-		if (item != null) {
-			item.setCandidadDisponible(item.getCandidadDisponible() - cantidadSolicitada);
-			inventario.save(item);
+			for (CatalogoIngredientes itemCatalogo : platillo.getIngredientes()) {
+				CatalogoIngredientes ingrediente = inventario.findById(itemCatalogo.getId()).orElse(null);
+				if (ingrediente != null) {
+					ingrediente.setCandidadDisponible(ingrediente.getCandidadDisponible() - item.getCantidad());
+					inventario.save(ingrediente);
+				}
+			}
 		}
 	}
-	
-	
 }
